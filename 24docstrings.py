1bd6b5d2495cd12c13c17ed1fb7248fd2ee498e2
Pronay Bhoumick <pronay910135bhoumick@gmail.com>
2023-12-22 17:02:36 +0530

    done

--- 24docstrings.py
+++ 24docstrings.py
@@ -8,8 +8,15 @@ def square(a):
     ''' it is used to calculate the square of a particular no
     '''
     return a*a; 
-
+def factorial(a):
+    '''I will calculate the factorial of a no 
+    '''
+    if(a==1 or a==0):
+        return 1; 
+    else :
+        return a*factorial(a-1);
 i=int(input()); 
 print("The square is", square(i)); 
 #to access
 print("the discription is",square.__doc__);   #__doc__ is used
+print("The description is ",factorial.__doc__,factorial(i)); 
